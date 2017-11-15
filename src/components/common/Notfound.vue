5c7e213a61695c77c20b65d872c04db1fca1c6fb
Andy <987670346@qq.com>
2017-11-15 13:50:45 +0800

    11.15 版本改动

--- src/components/common/Notfound.vue
+++ src/components/common/Notfound.vue
@@ -1,5 +1,28 @@
 <template>
  <div class="notfound">
-    <h2>404</h2>
+    <p class="sorry_box"><span class="sorry">抱歉</span>，您所访问的页面不存在...</p>
+    <p class="timeout"><span class="countdown">{{time}}</span>秒后自动跳转到<a href="/system/login">图灵智能交易系统</a></p>
  </div>
 </template>
+<script>
+ export default {
+    name: 'notfound',
+    data() {
+      return {
+		time:'5'
+      };
+    },
+    methods: {
+      
+    }
+}
+</script>
+<style scoped>
+.sorry_box{
+    margin:200px 0 15px;
+}
+.sorry_box .sorry{}
+.timeout{}
+.timeout .countdown{}
+
+</style>
